efe580fe3516b3df21c6b4a198146de55ba79198
itslach <s4130666@student.rmit.edu.au>
2024-07-23 23:32:51 +1000

    updating gif layout

--- routes/tailwind.config.js
+++ routes/tailwind.config.js
@@ -2,10 +2,11 @@ module.exports = {
     theme: {
       extend: {
         fontFamily: {
-          sans: ['"Anton SC"', 'sans-serif'],
+          sans: ['"Anton SC"', 'sans-serif'], // Ensure the font name is correct and wrapped in quotes if it contains spaces
         },
       },
     },
     variants: {},
     plugins: [],
   }
+  
