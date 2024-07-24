43afa0ca7c23839d0e2ff4c94ab559a072b26a01
itslach <s4130666@student.rmit.edu.au>
2024-07-24 11:38:11 +1000

    changing text colour

--- routes/tailwind.config.js
+++ routes/tailwind.config.js
@@ -2,6 +2,9 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   theme: {
     extend: {
+      colours: {
+        customLightGrey: 'd3d3d3',
+      }
       fontFamily: {
         sans: ['"Prompt"', '"Bebas Neue"', 'sans-serif'], // Include both fonts in the same array
       },
