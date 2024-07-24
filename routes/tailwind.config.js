276354c545cb4449ad50bee925b8687f8284981f
itslach <s4130666@student.rmit.edu.au>
2024-07-24 11:41:02 +1000

    adding #

--- routes/tailwind.config.js
+++ routes/tailwind.config.js
@@ -2,11 +2,11 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   theme: {
     extend: {
-      colours: {
-        customLightGrey: '#d3d3d3',
+      colors: {
+        customLightGrey: '#d3d3d3', 
       },
       fontFamily: {
-        sans: ['"Prompt"', '"Bebas Neue"', 'sans-serif'], // Include both fonts in the same array
+        sans: ['"Prompt"', '"Bebas Neue"', 'sans-serif'],
       },
     },
   },
